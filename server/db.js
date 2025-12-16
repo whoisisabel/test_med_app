d273a9603b6dd5e3d40a89414958286972d5ee5f
whoisisabel <isabel.mghendi@gmail.com>
2025-12-16 14:26:11 +0300

    Set up database connectivity

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:6YC5oCleTEEkdBAdx7osAojS@172.21.112.79:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
